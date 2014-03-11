1a35449b410931eea678e62a12ab2a06f80d5a6e
Josh Smith <git@joshsmithonline.com>
2014-03-11 22:52:43 +0000

    Removed some non utf-8 characters, added __init__.py

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from freqtools import *
--- freqTools.py
+++ freqTools.py
@@ -14,7 +14,7 @@ So we gots the following:
 
 2) mils_to_frequency(x)
   Receives an interval from the frequency
-  of root_pitch root_pitch[‘freq’]), expressed
+  of root_pitch root_pitch['freq']), expressed
   in mils. A positive value means the interval
   extends above root_pitch while a negative
   value means the interval extends below.
@@ -24,33 +24,33 @@ So we gots the following:
 3) freq_to_mils(x)
   Receives a frequency, returns interval in
   mils between that frequency and the
-  frequency of root_pitch (root_pitch[‘freq’]).
+  frequency of root_pitch (root_pitch['freq']).
   Returns positive value if given frequency
   is higher than root_pitch, negative if lower
 
 4) mils_to_note(x)
   Receives an interval from the frequency
-  of root_pitch root_pitch[‘freq’]), expressed
+  of root_pitch root_pitch['freq']), expressed
   in mils. A positive value means the interval
   extends above root_pitch while a negative
   value means the interval extends below.
   Returns a dictionary with three objects:
-      ‘note’ - C, D, F etc
-      ‘8ve’ - What octave the note is in.
+      note - C, D, F etc
+      8ve - What octave the note is in.
       Octaves run from C up to B, A4
       being concert A
-      ‘mils’ - how ‘sharp’ (positive value)
-      or ‘flat’ (negative value) the note is
+      mils - how 'sharp' (positive value)
+      or 'flat' (negative value) the note is
 
 5)freq_to_note(x)
   Receives frequency.
   Returns a dictionary with three objects:
-      ‘note’ - C, D, F etc
-      ‘8ve’ - What octave the note is in.
+      note - C, D, F etc
+      8ve - What octave the note is in.
       Octaves run from C up to B, A4
       being concert A
-      ‘mils’ - how ‘sharp’ (positive value)
-      or ‘flat’ (negative value) the note is
+      mils - how 'sharp' (positive value)
+      or 'flat' (negative value) the note is
 6)freq_to_valid_freq(x)
   Receives a frequency, returns the closest
   frequency with a valid (int) mils value
